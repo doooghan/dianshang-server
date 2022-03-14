553b14b8f678a7739feb6c4903e586aa880dd3bc
doooghan <doooghan@foxmail.com>
2022-03-14 14:34:55 +0800

    db配置

---
+++ src/db/db.js
@@ -0,0 +1,29 @@
+/**
+ * @description mongoose 连接数据库
+ */
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017'
+const dbName = 'testdb'
+
+// // 配置
+// mongoose.set('useCreateIndex', true)
+// mongoose.set('useFindAndModify', false)
+
+// 连接
+mongoose.connect(`${url}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', (err) => {
+  console.log('mongodb connect error', err)
+})
+
+db.once('open', () => {
+  console.log('success')
+})
+
+module.exports = mongoose
