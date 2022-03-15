5d56a6d9bf2c0b5624b1cf5c14024bcb7e51e84a
doooghan <doooghan@foxmail.com>
2022-03-15 19:04:49 +0800

    地址路由

--- src/controller/address.js
+++ src/controller/address.js
@@ -17,6 +17,47 @@ async function createAddress(username, data) {
   return newAddress
 }
 
+/**
+ * 获取收货地址列表
+ * @param {String} username 用户名
+ * @returns Address list
+ */
+async function getAddressList(username) {
+  const list = await Address.find({ username }).sort({ updateAt: -1 })
+  return list
+}
+
+/**
+ * 根据 id 获取单个收货地址
+ * @param {String} id
+ * @returns Address
+ */
+async function getAddressById(id) {
+  const address = await Address.findById(id)
+  return address
+}
+
+/**
+ * 更新地址
+ * @param {String} username 用户名
+ * @param {String} id 地址 id
+ * @param {Object} data 更新数据
+ * @returns new address
+ */
+async function updateAddress(username, id, data) {
+  const address = await Address.findOneAndUpdate(
+    { _id: id, username },
+    { username, ...data },
+    {
+      new: true, // 返回更新之后的最新数据（默认是false，返回修改前的数据）
+    }
+  )
+  return address
+}
+
 module.exports = {
   createAddress,
+  getAddressList,
+  getAddressById,
+  updateAddress,
 }
--- src/routes/address.js
+++ src/routes/address.js
@@ -4,7 +4,12 @@
 const router = require('koa-router')()
 const loginCheck = require('../middleware/loginCheck')
 const { SuccessModel, ErrorModel } = require('../res-model/index')
-const { createAddress } = require('../controller/address')
+const {
+  createAddress,
+  getAddressList,
+  getAddressById,
+  updateAddress,
+} = require('../controller/address')
 
 router.prefix('/api/user/address')
 
@@ -20,6 +25,38 @@ router.post('/', loginCheck, async (ctx, next) => {
   } catch (error) {
     console.error(error)
     ctx.body = new ErrorModel(10004, '新建收货地址失败')
+  }
+})
+
+// 获取收货地址列表
+router.get('/', loginCheck, async (ctx, next) => {
+  const userInfo = ctx.session.userInfo
+  const username = userInfo.username
+
+  const list = await getAddressList(username)
+
+  ctx.body = new SuccessModel(list)
+})
+
+//根据 id 获取单个收货地址，TODO：1.获取非本用户的地址，2.获取不存在的id
+router.get('/:id', loginCheck, async (ctx, next) => {
+  const id = ctx.params.id
+  const address = await getAddressById(id)
+  ctx.body = new SuccessModel(address)
+})
+
+// 更新收货地址
+router.patch('/:id', loginCheck, async (ctx, next) => {
+  const username = ctx.session.userInfo.username
+  const id = ctx.params.id
+  const data = ctx.request.body
+
+  try {
+    const address = await updateAddress(username, id, data)
+    ctx.body = new SuccessModel(address)
+  } catch (error) {
+    console.error(error)
+    ctx.body = new ErrorModel(10005, '更新收货地址失败')
   }
 })
 
